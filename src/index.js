0b3554fc20631b0125951ff940f25a405eec0c32
Guerth Manzala <guerthmanzala@gmail.com>
2026-02-04 11:03:33 +0100

    feat: implement Arcjet security for HTTP and WebSocket with rate limiting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { matchRouter } from "./routes/matches.js";
 import http from "http";
 import { attachWebsocketServer } from "./ws/server.js";
+import { securityMiddleware } from "./arcjet.js";
 
 const PORT = Number(process.env.PORT) || 8000;
 const HOST = process.env.HOST || "0.0.0.0";
@@ -17,6 +18,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Sportz API!" });
 });
+
+app.use(securityMiddleware());
 
 // Matches routes
 app.use("/matches", matchRouter);
